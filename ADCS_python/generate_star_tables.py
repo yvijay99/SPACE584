Fixed an error in generate_star_tables.py, that converted to radians twice, resulting in incorrect distance tables. star_tracker.py should be working again now, as it passed at least one test. The IR camera script still needs to be updated to use the raw array data.

ADCS_python/generate_star_tables.py
@@ -30,10 +30,10 @@ def main():
     pixel_table = np.loadtxt(input_file, delimiter=',', skiprows=1)
     
     # Index the first column of the pixel_table to get the phi positions
-    phi_positions = (pixel_table[:, 0]-x0) * np.pi/180 * img_wFOV/nwpx;
+    phi_positions = (pixel_table[:, 0]-x0) * img_wFOV/nwpx;
     
     # Index the second column of the pixel_table to get the theta positions
-    theta_positions = (pixel_table[:, 1]-y0) * np.pi/180 * img_hFOV/nhpx;
+    theta_positions = (pixel_table[:, 1]-y0) *img_hFOV/nhpx;
 
     # Combine the phi, theta, and brightness into the star_table
     star_table = np.array([phi_positions, theta_positions]).T
@@ -89,8 +89,8 @@ if __name__ == "__main__":
     img_hFOV = np.deg2rad(67) # Total field of view height of starfield reference image [rad]
     nwpx = 4608 # How many pixels wide the full starfield reference image is
     nhpx = 2592 # How many pixels wide the full starfield reference image is
-    x0 = 3270 # phi-pixel coord of reference to use at theta=0
-    y0 = 1608 # theta-pixel coord of rerence to use at theta = 0
+    x0 = 3303 # phi-pixel coord of reference to use at theta=0
+    y0 = 1546 # theta-pixel coord of rerence to use at theta = 0
     nangle = 10 # How many distances to neighbouring stars to compute per star
     
     # Run script

ADCS_python/star_tracker.py
@@ -213,6 +213,7 @@ def match_to_lookup(star_ls, star_pos_error, star_lookup, ax):
     fail_label = 'Failed Matches'
     
     while not star_match and istar < len(star_ls):
+        
         # Compute distances to other stars
         phi_diff = star_ls[istar, 0] - star_ls[:, 0]
         theta_diff = star_ls[istar, 1] - star_ls[:, 1]
@@ -225,21 +226,27 @@ def match_to_lookup(star_ls, star_pos_error, star_lookup, ax):
         # Drop the first element of sorted_angles (distance to itself = 0)
         sorted_angles = sorted_angles[1:]
 
+        print("Attempting to match istar",istar,"which has distances to neighbours:",sorted_angles[:5],"...")
+
         # Reduce sorted angles to only be as long as star_lookup is wide
-        num_lookup_angles = star_lookup.shape[1]
-        sorted_angles = sorted_angles[:min(len(sorted_angles), num_lookup_angles)]
+        #num_lookup_angles = star_lookup.shape[1]
+        #sorted_angles = sorted_angles[:min(len(sorted_angles), num_lookup_angles)]
 
         # Create an array of stars within the positional error range
         # Note: star_lookup[:, 0] is the first column of the lookup table
-        match_indices = (star_lookup[:, 0] >= (sorted_angles[0] - star_pos_error)) & \
-                        (star_lookup[:, 0] <= (sorted_angles[0] + star_pos_error))
+        match_indices = (star_lookup[:, 0] >= (sorted_angles[0] - star_pos_error)) & (star_lookup[:, 0] <= (sorted_angles[0] + star_pos_error))
         
         matched_stars = star_lookup[match_indices, :]
+        
+        print("Stars in catalog with similar distances:")
+        
+        for matched_star in np.where(match_indices)[0]:
+            print('Star #',matched_star,"with distances:", star_lookup[matched_star,:5],"...")
 
         # Check if that immediately identified the current state. 
         # If not, compare the angular distances one by one, bringing in more to
         # reduce the number of remaining options
-        iangle = 1 # Python uses 0-based indexing, so iangle=1 corresponds to the 2nd angle
+        iangle = 0 # Python uses 0-based indexing, so iangle=1 corresponds to the 2nd angle
 
         while matched_stars.shape[0] > 1 and iangle < star_lookup.shape[1]:
             print('Found multiple matches, refining to next angle')
@@ -303,7 +310,7 @@ if __name__ == "__main__":
     FOV = np.pi/6
     img_wFOV = np.deg2rad(102) # Total field of view width of starfield reference image [rad]
     nwpx = 4608 # How many pixels wide the full starfield reference image is
-    star_pos_error = 20*img_wFOV/nwpx;
+    star_pos_error = 2*img_wFOV/nwpx;
     
     savePlot = True
     